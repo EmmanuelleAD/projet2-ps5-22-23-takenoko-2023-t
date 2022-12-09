9f3d188446771f104d89fd7ed1160334692a34c9
AchehbouneMohammedAmine <mohammed-amine.achehboune@etu.univ-cotedazur.fr>
2022-12-09 15:26:06 +0100

    ajout de la classe joueur#1

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Joueur.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Joueur.java
@@ -1,2 +1,24 @@
-package fr.cotedazur.univ.polytech.startingpoint;public class Joueur {
+
+package fr.cotedazur.univ.polytech.startingpoint;
+
+import java.util.ArrayList;
+
+public class Joueur {
+    public String nom;
+    public Parcelle[] tabParcelle;
+    ArrayList list = new ArrayList();
+
+    public Joueur(String nom, Parcelle[] tabParcelle) {
+
+
+        this.nom = nom;
+        this.tabParcelle = tabParcelle;
+
+
+    }
+    public void addParcelle(Parcelle parcelle){
+        list.add(parcelle);
+
+    }
+dasdasdasdasdasdsadas
 }
