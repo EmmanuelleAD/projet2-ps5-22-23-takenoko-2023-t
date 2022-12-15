75fb94e5af83b84a915a9a73d91c3eddab5b4e7d
Wassim Baratli <wassim.baratli@etu.univ-cotedazur.fr~>
2022-12-15 16:28:03 +0100

    feat : Ajout main #7

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Main.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Main.java
@@ -1,2 +1,16 @@
-package fr.cotedazur.univ.polytech.startingpoint;public class main {
+package fr.cotedazur.univ.polytech.startingpoint;
+
+
+
+public class Main {
+
+        public static String hello() {
+            return "Hello World!";
+        }
+
+        public static void main(String... args) {
+            System.out.println(hello());
+        }
+
 }
+
