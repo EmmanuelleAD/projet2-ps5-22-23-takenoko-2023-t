f5baf9a22dbca249cfad920ce03d2bdcb0b9e67c
Wassim Baratli <wassim.baratli@etu.univ-cotedazur.fr~>
2023-01-13 16:17:02 +0100

    feat : Ajout classe Cerveau #20

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Cerveau.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Cerveau.java
@@ -1,2 +1,28 @@
-package fr.cotedazur.univ.polytech.startingpoint;public class Cerveau {
+package fr.cotedazur.univ.polytech.startingpoint;
+
+import java.util.List;
+
+public class Cerveau {
+    public Joueur joueur;
+
+    public Cerveau(Joueur joueur) {
+        this.joueur=joueur;
+    }
+
+
+
+
+    public Action decider(Jeu jeu){
+            if (joueur.getCartesObjectifs().size()==0){
+                List<ObjectifParcelle> op = jeu.getObjectifsParcelles();
+                Objectif newObjectif = op.get(op.size()-1);
+                ActionPiocher newAction= new ActionPiocher(newObjectif);
+        }
+        return null;
+    }
+
+
+
+
+
 }
