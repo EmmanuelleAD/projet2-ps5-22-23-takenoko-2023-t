68ce3779b23193075a9a7a466fae347e18ebeea0
Wassim Baratli <wassim.baratli@etu.univ-cotedazur.fr~>
2023-01-20 14:17:42 +0100

    Ajout test classe ActionJardinier et classe Parcelle #29

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/ActionJardinierTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/ActionJardinierTest.java
@@ -1,4 +1,21 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ActionJardinierTest {
-  
+
+    @Test
+    void getDescription() {
+        Joueur joueur1 = new Joueur(1.85, "Wassim");
+        Joueur joueur2 = new Joueur(1.6, "Brahim");
+        Jeu jeu = new Jeu(joueur1, joueur2);
+        jeu.initialisation();
+        jeu.jouer();
+        Position position = new Position(1, 1);
+        ActionJardinier actionJardinier = new ActionJardinier(new Parcelle(position));
+        String comment = " a éffectué une action Jardinier en " + position.toString();
+        assertEquals(comment , actionJardinier.getDescription());
+    }
 }
