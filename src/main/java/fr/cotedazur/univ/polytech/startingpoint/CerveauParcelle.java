1e7c525292e475d1c81ea3176457af0768d49526
Wassim Baratli <wassim.baratli@etu.univ-cotedazur.fr~>
2023-01-20 16:05:22 +0100

    Modif classe Cerveau #20

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/CerveauParcelle.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/CerveauParcelle.java
@@ -1,2 +1,25 @@
-package fr.cotedazur.univ.polytech.startingpoint;public class CerveauParcelle {
+package fr.cotedazur.univ.polytech.startingpoint;
+
+import java.util.List;
+
+public class CerveauParcelle extends Cerveau {
+
+    public CerveauParcelle(Joueur joueur) {
+        super(joueur);
+    }
+
+    public Action decider(Jeu jeu){
+        if (joueur.getCartesObjectifs().size()==0){
+            List<ObjectifParcelle> op = jeu.getObjectifsParcelles();
+            Objectif newObjectif = op.get(op.size()-1);
+            ActionPiocher newAction= new ActionPiocher(newObjectif);
+            return newAction;
+
+        }
+
+        List<Position> listPlacement = jeu.getPlacementsPossibles();
+        return new ActionParcelle(new Parcelle( listPlacement.get(0)));
+    }
+
+
 }
