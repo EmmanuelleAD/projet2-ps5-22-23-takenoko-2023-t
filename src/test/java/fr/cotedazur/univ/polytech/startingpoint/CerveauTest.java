ed51c64c0e5e6b61b1ebf0b895eb4e330f508d92
Wassim Baratli <wassim.baratli@etu.univ-cotedazur.fr~>
2023-01-20 15:26:21 +0100

    Ajout test classe ActionJardinier et classe Parcelle #29

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/CerveauTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/CerveauTest.java
@@ -34,5 +34,17 @@ class CerveauTest {
 
     @Test
     void deciderJardinier() {
-    }
+           Joueur joueur1 = new Joueur(1.85, "Wassim");
+            Joueur joueur2 = new Joueur(1.6, "Brahim");
+            Jeu jeu = new Jeu(joueur1, joueur2);
+            jeu.initialisation();
+            jeu.jouer();
+            Cerveau cerveau = new Cerveau(joueur1);
+            cerveau.deciderJardinier(jeu);
+            assertEquals(3, cerveau.deciderJardinier(jeu).getNbreAction());
+
+        }
+
+
+
 }
