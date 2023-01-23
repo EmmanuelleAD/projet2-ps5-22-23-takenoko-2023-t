90a78fa373025fea605b8c01d2b7b10419d0a692
Wassim Baratli <wassim.baratli@etu.univ-cotedazur.fr~>
2023-01-23 15:52:54 +0100

    feat : Ajout test inteligence #23

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/ObjectifParcelle.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/ObjectifParcelle.java
@@ -33,9 +33,9 @@ public class ObjectifParcelle extends Objectif {
             case "POUSSB":
                 return verifierValiderPOUSSB(parcellesSansEtang);
             case "ALI3":
-                return verifierValiderAli3(parcellesSansEtang);
+                return verifierValiderAli3(parcellesSansEtang).isManquant;
             case "PARC":
-                return verifierValiderPARC(parcellesSansEtang);
+                return verifierValiderPARC(parcellesSansEtang).isManquant;
             case "GRP3":
                 return verifierValiderGRP3(parcellesSansEtang);
             case "GRP4":
@@ -81,39 +81,95 @@ public class ObjectifParcelle extends Objectif {
         return false;
     }
 
-    private boolean verifierValiderPARC(List<Parcelle> parcelles) {
+    private ObjectifVerifier verifierValiderPARC(List<Parcelle> parcelles) {
         List<Position> positions = parcelles.stream().map(Parcelle::getPosition).collect(Collectors.toList());
+        ObjectifVerifier objectif = new ObjectifVerifier(false, new ArrayList<>());
         for (Position p : positions
         ) {
-            if ((p.y%2==0)&&positions.contains(new Position(p.x , p.y + 2)) && (positions.contains(new Position(p.x, p.y + 1))||positions.contains(new Position(p.x+1,p.y+1)) ))
-                return true;
-            if ((p.y%2!=0)&&(positions.contains(new Position(p.x-1 , p.y + 1))||positions.contains(new Position(p.x , p.y + 1))) && (positions.contains(new Position(p.x, p.y + 2)) ))
-                return true;
+
+            List<Position> manquant = new ArrayList<>();
+            if (p.y % 2 == 0) {
+
+                if ((p.y % 2 == 0) && positions.contains(new Position(p.x, p.y + 2)) && (positions.contains(new Position(p.x, p.y + 1)) || positions.contains(new Position(p.x + 1, p.y + 1)))) {
+                    objectif.setIsManquant(true);
+                    return objectif;
+                }
+
+                if (!positions.contains(new Position(p.x, p.y + 2))) {
+                    manquant.add(new Position(p.x, p.y + 2));
+                }
+                if (!positions.contains(new Position(p.x, p.y + 1)) && !positions.contains(new Position(p.x + 1, p.y + 1))) {
+                    manquant.add(new Position(p.x, p.y + 1));
+                    manquant.add(new Position(p.x + 1, p.y + 1));
+                }
+                objectif.getParcellesManquant().add(manquant);
+            }
+
+
+            if ((p.y % 2 != 0) && (positions.contains(new Position(p.x - 1, p.y + 1)) || positions.contains(new Position(p.x, p.y + 1))) && (positions.contains(new Position(p.x, p.y + 2)))) {
+                objectif.setIsManquant(true);
+                return objectif;
+            }
+
+            if (!positions.contains(new Position(p.x, p.y + 2))) {
+                manquant.add(new Position(p.x, p.y +2));
+            }
+            if (!positions.contains(new Position(p.x-1, p.y+1 )) && !positions.contains(new Position(p.x , p.y+1 ))) {
+                manquant.add(new Position(p.x-1, p.y+1));
+                manquant.add(new Position(p.x, p.y+1 ));
+            }
+            objectif.getParcellesManquant().add(manquant);
         }
 
-        return false;
+        return objectif;
     }
 
-    private boolean verifierValiderAli3(List<Parcelle> parcelles) {
+
+
+    private ObjectifVerifier verifierValiderAli3(List<Parcelle> parcelles) {
+        ObjectifVerifier objectif = new ObjectifVerifier(false, new ArrayList<>());
         List<Position> positions = parcelles.stream().map(Parcelle::getPosition).collect(Collectors.toList());
         for (Position p : positions
         ) {
+            List<Position> manquant = new ArrayList<>();
             if ((p.getY()%2==0)){
-                if(positions.contains(new Position(p.x + 1, p.y + 2)) && (positions.contains(new Position(p.x+1, p.y + 1))  ))
-                return true;
-                else if((positions.contains(new Position(p.x , p.y + 1)) && (positions.contains(new Position(p.x-1, p.y + 2))  ))){
-                    return  true;
-
+                if(positions.contains(new Position(p.x + 1, p.y + 2)) && (positions.contains(new Position(p.x+1, p.y + 1))  )){
+                    objectif.setIsManquant(true);
+                    return objectif;
                 }
+
+                if (!positions.contains(new Position(p.x+1, p.y + 2))) {
+                    manquant.add(new Position(p.x+1, p.y + 2));
+                }
+
+                if ( !positions.contains(new Position(p.x + 1, p.y + 1))) {
+                    manquant.add(new Position(p.x + 1, p.y + 1));
+                }
+
+                objectif.getParcellesManquant().add(manquant);
             }
             if ((p.getY()%2!=0)){
-                if(positions.contains(new Position(p.x + 1, p.y + 2)) && (positions.contains(new Position(p.x, p.y + 1))  ))
-                return true;
-                else if(positions.contains(new Position(p.x - 1, p.y + 1)) && (positions.contains(new Position(p.x-1, p.y + 2)) )) return true;
+                if(positions.contains(new Position(p.x + 1, p.y + 2)) && (positions.contains(new Position(p.x, p.y + 1))  )){
+                    objectif.setIsManquant(true);
+                    return objectif;
+                }
+                if (!positions.contains(new Position(p.x+1, p.y + 2))) {
+                    manquant.add(new Position(p.x+1, p.y + 2));
+                }
+                if ( !positions.contains(new Position(p.x , p.y + 1))) {
+                    manquant.add(new Position(p.x , p.y + 1));
+                }
+
+                else if(positions.contains(new Position(p.x - 1, p.y + 1)) && (positions.contains(new Position(p.x-1, p.y + 2)) )){
+                    objectif.setIsManquant(true);
+                    return objectif;
+                }
+
+                objectif.getParcellesManquant().add(manquant);
             }
 
         }
-        return false;
+        return objectif;
 
 
     }
@@ -123,6 +179,7 @@ public class ObjectifParcelle extends Objectif {
         return false;
     }
 
+
     private boolean verifierValiderPARADJ(List<Parcelle> parcelles) {
         for (Parcelle p:parcelles
              ) {
@@ -131,6 +188,7 @@ public class ObjectifParcelle extends Objectif {
         }
         return false;
     }
+
 
 
 }
--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/ObjectifVerifier.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/ObjectifVerifier.java
@@ -1,2 +1,39 @@
-package fr.cotedazur.univ.polytech.startingpoint;public class ObjectifVerifier {
+package fr.cotedazur.univ.polytech.startingpoint;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObjectifVerifier {
+    boolean isManquant=false;
+    List parcellesManquant = new ArrayList();
+
+
+    public ObjectifVerifier(boolean isManquant, List parcellesManquant) {
+        this.isManquant = isManquant;
+        this.parcellesManquant = parcellesManquant;
+    }
+
+
+    public ObjectifVerifier() {
+        this.isManquant=false;
+        this.parcellesManquant = new ArrayList<>();
+    }
+
+
+
+    public boolean isManquant() {
+        return isManquant;
+    }
+
+    public List getParcellesManquant() {
+        return parcellesManquant;
+    }
+
+
+    public void setIsManquant(boolean isManquant) {
+        this.isManquant=isManquant;
+    }
+
+
+
 }
