919e4ac8920b3fe87220f2e181d83621ef984df5
Emmanuelle <adjo-emmanuelle.adote@etu.univ-cotedazur.fr>
2023-02-01 22:09:01 +0100

    revert : ajout de la classe score

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Score.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Score.java
@@ -1,21 +1,2 @@
 package fr.cotedazur.univ.polytech.startingpoint;public class Score {
-    private String joueur;
-    private int jeuGagne = 0;
-    private int jeuperdu = 0;
-    private int nul = 0;
-    private int totalScore = 0;
-    private int nbjeu = 0;
-
-
-
-    public Score(String joueur) {
-        this.joueur = joueur;
-    }
-    public void Scorelive(boolean joueurgagne, boolean egalite, int score) {
-        jeuGagne += !egalite && joueurgagne ? 1 : 0;
-        jeuperdu += !egalite && !joueurgagne ? 1 : 0;
-        nul += egalite ? 1 : 0;
-        totalScore += score;
-        nbjeu++;
-}
 }
