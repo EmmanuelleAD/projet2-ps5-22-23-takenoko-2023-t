bfb6762ee83c18034734bd6c680bc0c82573e628
AchehbouneMohammedAmine <mohammed-amine.achehboune@etu.univ-cotedazur.fr>
2023-02-02 11:09:12 +0100

    feat: ajout de deplacement en ligne droite#25

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/DeplacementPanda.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/DeplacementPanda.java
@@ -1,2 +1,25 @@
-package fr.cotedazur.univ.polytech.startingpoint;public class DeplacementPanda {
+package fr.cotedazur.univ.polytech.startingpoint;
+
+public class DeplacementPanda extends Action {
+
+
+    public DeplacementPanda(String player, Personnage panda) {
+        super(player,panda);
+    }
+
+    public DeplacementPanda(String nomAction, String description) {
+        super(nomAction, description);
+    }
+
+
+    @Override
+    public boolean isActionDecrementedFromPlayer() {
+        return true;
+    }
+
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
 }
