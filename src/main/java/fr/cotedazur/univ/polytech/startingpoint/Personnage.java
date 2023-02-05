4e2b358bda62b23f20fa620ddaa4d9e1513df9d5
Emmanuelle <adjo-emmanuelle.adote@etu.univ-cotedazur.fr>
2023-02-05 12:12:37 +0100

    feat : création d'une méthode minimale de déplacement pour un personnage #25

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Personnage.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Personnage.java
@@ -3,26 +3,22 @@ package fr.cotedazur.univ.polytech.startingpoint;
 import java.util.List;
 import java.util.Optional;
 
-public abstract class Personnage{
+public abstract class Personnage {
+    protected PersonnageName nom;
 
     protected Position position;
 
-    public Personnage(String panda) {
+    public Personnage() {
     }
 
 
-    public PersonnageName getName() {
-        return PersonnageName.Jardinier;
-    }
+    public abstract PersonnageName getName();
 
     public abstract Optional<Bambou> move(Parcelle parcelle, List<Parcelle> parcelles);
+    public void deplacer(Parcelle parcelle,List<Parcelle>parcelles){
+        if(parcelles.contains(parcelle)) this.position=parcelle.getPosition();
 
-    public PersonnageName getNom() {
-        return nom;
     }
-
-    protected PersonnageName nom;
-
 
 
     public Position getPosition() {
