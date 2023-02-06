b4bb173f680b34a8aa223a44eedfd8119506136d
Wassim Baratli <wassim.baratli@etu.univ-cotedazur.fr~>
2023-02-06 12:04:16 +0100

    feat : Ajout classe CerveauLivraison #42

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/CerveauLivraisonTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/CerveauLivraisonTest.java
@@ -1,4 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
+package fr.cotedazur.univ.polytech.startingpoint;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CerveauLivraisonTest {
-  
+
+    Parcelle p11=new Parcelle(new Position(1,1));
+    Joueur joueur=new Joueur(1.7,"Mohamed");
+    Joueur joueurSaboteur =new Joueur(1.6,"Emmanuelle");
+
+    CerveauLivraison cerveauLivraison=new CerveauLivraison(joueurSaboteur);
+
+    Jeu jeu=new Jeu(joueurSaboteur,joueur);
+    @BeforeEach
+    void setUp(){
+        joueurSaboteur.setCerveau(new CerveauLivraison(joueurSaboteur));
+        jeu.initialisation();
+    }
+
+
 }
