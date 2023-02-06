b49c1099b4d86ab948b53aa5705608ce3e60f462
Emmanuelle <adjo-emmanuelle.adote@etu.univ-cotedazur.fr>
2023-02-06 16:37:38 +0100

    feat : exéctuion du main avec l'option 2000 #22

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Main.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Main.java
@@ -1,20 +1,30 @@
 package fr.cotedazur.univ.polytech.startingpoint;
-
-
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import java.util.Arrays;
 
 public class Main {
-    static int nbEgalite=0;
+    @Parameter(names={"--2thousands"})
+    boolean typeExec=false;
 
-    public static void main(String[] args) {
-        Joueur joueur1 = new Joueur(1.62, "Wassim");
-        joueur1.setCerveau(new CerveauParcelle(joueur1));
-        Joueur joueur3 = new Joueur(1.5, "Emmanuelle");
-        joueur3.setCerveau(new CerveauParcelle(joueur3));
-        mainMille(joueur1,joueur3);
-        System.out.println("nb de partie gagnees par " + joueur1.getNom()+" est "+joueur1.partieGagnees);
-        System.out.println("nb de partie gagnees par " + joueur3.getNom()+" est "+joueur3.partieGagnees);
-        System.out.println("nb de partie egalite est " +nbEgalite);
+    static int nbEgalite=0;
+    public static void main(String ...args){
+
+        Main mainParametre=new Main();
+        JCommander.newBuilder().addObject(mainParametre).build().parse(args);
+        mainParametre.run();
+    }
+
+    public  void main() {
+        Joueur joueur1 = new Joueur(1.85, "Wassim");
+        joueur1.setCerveau(new CerveauJardinier(joueur1));
+        Joueur joueur2 = new Joueur(1.6, "Brahim");
+        joueur2.setCerveau(new CerveauParcelle(joueur2));
+        Joueur joueur3=new Joueur(1.65,"Emmanuelle");
+        joueur3.setCerveau(new CerveauPanda(joueur3));
+        Jeu jeu = new Jeu(Arrays.asList(joueur1, joueur2,joueur3));
+        jeu.initialisation();
+        jeu.jouer();
     }
 
 
@@ -31,6 +41,36 @@ public class Main {
                 nbEgalite++;
             }
         }
+        for (Joueur joueur:joueurs
+             ) {
+            System.out.println("nb de partie gagnées par " + joueur.getNom()+" est "+joueur.partieGagnees);
+            joueur.partieGagnees=0;
+
+
+        }
+        System.out.println("nb de partie égalité est " +nbEgalite);
+
+
     }
+    public  void run() {
+        if(typeExec)
+        {
+            Joueur joueur1 = new Joueur(1.62, "Wassim");
+            joueur1.setCerveau(new CerveauParcelle(joueur1));
+            Joueur joueur3 = new Joueur(1.5, "Emmanuelle");
+            joueur3.setCerveau(new CerveauParcelle(joueur3));
+            mainMille(joueur1,joueur3);
+            joueur3.setCerveau(new CerveauPanda(joueur3));
+            mainMille(joueur1,joueur3);
+
+
+        }
+        else main();
+
+
+
+    }
+
+
 }
 
