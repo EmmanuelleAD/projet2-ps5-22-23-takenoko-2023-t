86ac794bf513b46d662af1c51d224a0349acef7c
Wassim Baratli <wassim.baratli@etu.univ-cotedazur.fr~>
2023-02-08 11:01:39 +0100

    feat : correction bugs #50

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/ActionJardinier.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/ActionJardinier.java
@@ -1,72 +1,50 @@
 package fr.cotedazur.univ.polytech.startingpoint;
 
 
-
 import java.util.Objects;
 
 public class ActionJardinier extends Action{
-        public Parcelle getParcelle() {
-            return parcelle;
-        }
+    public Parcelle getParcelle() {
+        return parcelle;
+    }
 
-        private Parcelle parcelle;
-        public ActionJardinier(Parcelle p) {
-            super(Type.TypeJardinier.getNomType(), "");
-            this.parcelle=p;
+    private final Parcelle parcelle;
+    public ActionJardinier(Parcelle p) {
+        super(Type.TypeJardinier.getNomType(), "");
+        this.parcelle=p;
 
-        }
+    }
 
-<<<<<<< HEAD
-        @Override
-        public String getDescription() {
-            String msg=(parcelle.bambou.isPresent())?". Le bambou a une taille actuelle de "+parcelle.getTaille():"";
-            return " a éffectué une action Jardinier en " +parcelle.getPosition()+msg;
+    @Override
+    public String getDescription() {
+        String msg=(parcelle.bambou.isPresent())?". Le bambou a une taille actuelle de "+parcelle.getTaille():"";
+        return " a éffectué une action Jardinier en " +parcelle.getPosition()+msg;
+    }
+
+    @Override
+    public boolean traiter(Joueur joueur, Jeu jeu) {
+        if(!Position.isStraightMovement(jeu.getJardinier().getPosition(), this.parcelle.getPosition())) {
+            throw new IllegalArgumentException("Le jardinier peut seulement se déplacer en ligne droite ! ");
         }
-=======
-  /*  @Override
+        jeu.getJardinier().move(this.getParcelle(),jeu.getParcellesPlacees());
+
+        Jeu.logger.info(joueur.getNom() + this.getDescription());
+        return true;
+
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
->>>>>>> 6910a8743ddff39b9fa23c22b89a64461392a36f
-
-        @Override
-        public boolean traiter(Joueur joueur, Jeu jeu) {
-            if(!Position.isStraightMovement(jeu.getJardinier().getPosition(), this.parcelle.getPosition())) {
-                throw new IllegalArgumentException("Le jardinier peut seulement se déplacer en ligne droite ! ");
-            }
-            jeu.getJardinier().move(this.getParcelle(),jeu.getParcellesPlacees());
-
-            Jeu.logger.info(joueur.getNom() + this.getDescription());
-            return true;
-
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            if (!super.equals(o)) return false;
-
-            ActionJardinier that = (ActionJardinier) o;
-
-            return Objects.equals(parcelle, that.parcelle);
-        }
-
-        @Override
-        public int hashCode() {
-            int result = super.hashCode();
-            result = 31 * result + (parcelle != null ? parcelle.hashCode() : 0);
-            return result;
-        }
-
-<<<<<<< HEAD
-=======
+        ActionJardinier that = (ActionJardinier) o;
+        return Objects.equals(parcelle, that.parcelle);
+    }
     @Override
     public int hashCode() {
         int result = super.hashCode();
         result = 31 * result + (parcelle != null ? parcelle.hashCode() : 0);
         return result;
-    }*/
->>>>>>> 6910a8743ddff39b9fa23c22b89a64461392a36f
+    }
 }
